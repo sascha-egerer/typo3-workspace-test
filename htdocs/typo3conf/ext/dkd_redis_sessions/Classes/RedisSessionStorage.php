18747ef552bf5b287f9940dfcf0813dfaef20416
Thorsten Kahler <thorsten.kahler@dkd.de>
2013-09-27 16:36:40 +0200

    [Refactor] Use subclasses to distunguish subtypes
    * new classes BackendStorage and FrontendStorage extend now abstract
    class RedisSessionStorage
    * check proper subtype in init() method of subclasses

--- htdocs/typo3conf/ext/dkd_redis_sessions/Classes/RedisSessionStorage.php
+++ htdocs/typo3conf/ext/dkd_redis_sessions/Classes/RedisSessionStorage.php
@@ -32,7 +32,7 @@ use \TYPO3\CMS\Core\Cache\Backend\RedisBackend;
  * Implements Redis as \TYPO3\CMS\Core\Session\StorageInterface
  * @package TYPO3\CMS\DkdRedisSessions
  */
-class RedisSessionStorage extends \TYPO3\CMS\Core\Service\AbstractService implements Session\StorageInterface {
+abstract class RedisSessionStorage extends \TYPO3\CMS\Core\Service\AbstractService implements Session\StorageInterface {
 
 	/**
 	 * @var RedisBackend $backend the Redis CacheBackend
@@ -43,40 +43,31 @@ class RedisSessionStorage extends \TYPO3\CMS\Core\Service\AbstractService implem
 	 * @var string $subtype the service subtype
 	 */
 	protected $subtype;
+
+
 	/**
 	 * Connect to Redis DB
 	 * @return bool|void
 	 */
 	public function init() {
-		switch($this->info['requestedServiceSubType']) {
-			case 'frontend':
-			case 'backend':
-				$subtype = $this->info['requestedServiceSubType'];
-				break;
-			default:
+		$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['dkd_redis_sessions']);
+		list($hostname, $port) = GeneralUtility::trimExplode(':', $extConf[$this->subtype . '_server']);
+		$this->backend = GeneralUtility::makeInstance(
+			'TYPO3\\CMS\\Core\\Cache\\Backend\\RedisBackend',
+			'',
+			array(
+				'hostname' => $hostname,
+				'port' => $port,
+				'database' => intval($extConf[$this->subtype . '_db'])
+			)
+		);
+		if ($this->backend instanceof RedisBackend) {
+			try {
+				$this->backend->initializeObject();
+			}
+			catch(\TYPO3\CMS\Core\Cache\Exception $e) {
+				GeneralUtility::sysLog($e->getMessage(), 'dkd_redis_sessions', GeneralUtility::SYSLOG_SEVERITY_WARNING);
 				return FALSE;
-		}
-
-		if ($this->subtype != $subtype) {
-			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['dkd_redis_sessions']);
-			list($hostname, $port) = GeneralUtility::trimExplode(':', $extConf[$subtype . '_server']);
-			$this->backend = GeneralUtility::makeInstance(
-				'TYPO3\\CMS\\Core\\Cache\\Backend\\RedisBackend',
-				'',
-				array(
-					'hostname' => $hostname,
-					'port' => $port,
-					'database' => intval($extConf[$subtype . '_db'])
-				)
-			);
-			if ($this->backend instanceof RedisBackend) {
-				try {
-					$this->backend->initializeObject();
-				}
-				catch(\TYPO3\CMS\Core\Cache\Exception $e) {
-					GeneralUtility::sysLog($e->getMessage(), 'dkd_redis_sessions', GeneralUtility::SYSLOG_SEVERITY_WARNING);
-					return FALSE;
-				}
 			}
 		}
 		return TRUE;
--- htdocs/typo3conf/ext/dkd_redis_sessions/ext_localconf.php
+++ htdocs/typo3conf/ext/dkd_redis_sessions/ext_localconf.php
@@ -7,16 +7,33 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
 	$_EXTKEY,
 	'sessionStorage',
-	'TYPO3\\CMS\\DkdRedisSessions\\RedisSessionStorage',
+	'TYPO3\\CMS\\DkdRedisSessions\\FrontendSessionStorage',
 	array(
 		'title' => 'Session Storage: Redis',
 		'description' => 'Stores user sessions in Redis (http://redis.io)',
-		'subtype' => 'backend, frontend',
+		'subtype' => 'frontend',
 		'available' => TRUE,
 		'priority' => 100,
 		'quality' => 50,
 		'os' => '',
 		'exec' => '',
-		'className' => 'TYPO3\\CMS\\DkdRedisSessions\\RedisSessionStorage'
+		'className' => 'TYPO3\\CMS\\DkdRedisSessions\\FrontendStorage'
+	)
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+	$_EXTKEY,
+	'sessionStorage',
+	'TYPO3\\CMS\\DkdRedisSessions\\BackendStorage',
+	array(
+		'title' => 'Session Storage: Redis',
+		'description' => 'Stores user sessions in Redis (http://redis.io)',
+		'subtype' => 'backend',
+		'available' => TRUE,
+		'priority' => 100,
+		'quality' => 50,
+		'os' => '',
+		'exec' => '',
+		'className' => 'TYPO3\\CMS\\DkdRedisSessions\\BackendStorage'
 	)
 );
